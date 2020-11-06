3df50a8d6691c0403eccc05ac5e2888e6181bf2c
HN-00 <h.naoki00@gmail.com>
2020-11-06 11:54:45 +0900

    @yield()を追加とか...... #22

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -7,6 +7,8 @@
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+        <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+
 
         <style>
             header {
@@ -44,6 +46,14 @@
                 margin: 0; 
                 padding: 0;
             }
+            .navbar-brand-center {
+                position: absolute;
+                width: 100%;
+                left: 0;
+                top: 0;
+                text-align:center;
+                margin: auto;
+            }
         
         </style>
     </head>
@@ -54,51 +64,59 @@
     <body>
 
         <nav class="navbar navbar-expand-sm navbar-dark bg-dark mt-0 mb-0 pt-0 pb-0 sticky-top">
+
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav4" aria-controls="navbarNav4" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <a class="navbar-brand" href="#">Spotlight</a>
+
+            <a class="navbar-brand navbar-brand-center" href="#">Spotlight</a>
             <div class="collapse navbar-collapse">
-                {{-- <ul class="navbar-nav">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#">新規登録</a>
+                <ul class="navbar-nav">
+                    <li class="nav-item activie">
+                        <a class="nav-link" href="#">sign in</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">データ一覧</a>
+                        <a class="nav-link" href="#">sign up</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">エクスポート</a>
+                        <a class="nav-link" href="#">my page</a>
                     </li>
-                </ul> --}}
+                </ul>
             </div>
-            <form class="form-inline mt-1 mb-1">
+
+            <form class="form-inline mt-1 mb-1 align-right">
                 <input class="form-control mr-sm-1" type="search">
                 <button class="btn btn-primary" type="submit">検索</button>
             </form>
+
         </nav>
         
 
         <div class="container">
 
             <div class="top">
-                <p>tekitounna. bunsyou</p>
+                <img src="http://placehold.jp/200x200.png" class="rounded-circle">
+                <h1>zyugemu zyugemu</h1>
             </div>
 
-            <div class="tab">
+            <div class="tab w-100 nav-justified">
                 <ul class="nav nav-tabs">
                     <li class="nav-item">
-                        <a class="nav-link active">リンク1</a>
+                        <a href="spotlight/user/:id/profile" class="nav-link active">プロフィール</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link">リンク2</a>
+                        <a href="spotlight/user/:id/locate" class="nav-link">ロケーション</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link">リンク3</a>
+                        <a href="spotlight/user/:id/goods" class="nav-link">グッズ</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link">リンク4</a>
+                        <a href="spotlight/user/:id/sample" class="nav-link">サンプルリンク</a>
                     </li>
                 </ul>
+
+                <div>@yield('profile')</div>
+                
             </div>
 
             <br>

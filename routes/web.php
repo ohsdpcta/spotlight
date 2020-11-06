44fa2553fd27ad3d301b05afdfe868a4001d8717
gekogekominimi <chousokabe1949556@gmail.com>
2020-11-06 13:33:50 +0900

    #24 検索機能

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,5 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/hello', 'UserController@index');
+Route::get('/', 'UserController@index');
+Route::post('/search', 'UserController@search');

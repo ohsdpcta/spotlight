caa292b35ebcc4059308200abe86d34f0d853944
UY0409 <utikawa1949592@gmail.com>
2020-11-06 10:31:24 +0900

    #23 signup.blade.phpの変更

--- resources/views/user/signup.blade.php
+++ resources/views/user/signup.blade.php
@@ -1,14 +1,20 @@
-@section('新規登録画面')
+<!DOCTYPE html>
+<head>
+    <title>新規登録画面</title>
+</head>
 
-@endsection
-@section('新規登録')
-    <form action="{{ route('user.signup') }}" method="POST">
-    <table>
-        @csrf
-        <tr><th>ユーザー名</th><td><input type="text" name="user_name" value="{{old('user_name')}}"></td></tr>
-        <tr><th>メールアドレス</th><td><input type="email" name="email" value="{{old('email')}}"></td></tr>
-        <tr><th>パスワード</th><td><input type="password" name="password" ></td></tr>
-        <tr><th>パスワード確認</th><td><input type="password" name="password_Confirmation" ></td></tr>
-    </table>
+
+
+<h1>新規登録</h1>
+    <form action="/user/signup" method="POST">
+        <table>
+            @csrf
+            <tr><th>ユーザー名</th><td><input type="text" name="user_name" value="{{old('user_name')}}"></td></tr>
+            <tr><th>メールアドレス</th><td><input type="email" name="email" value="{{old('email')}}"></td></tr>
+            <tr><th>パスワード</th><td><input type="password" name="password" ></td></tr>
+            <tr><th>パスワード確認</th><td><input type="password" name="password_Confirmation" ></td></tr>
+            {{-- 各種ボタン --}}
+            <tr><th></th><td><input type="submit" value="登録"></td></tr>
+        </table>
     </form>
-@endsection
+

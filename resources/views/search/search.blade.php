0e913334bcafc051428aec74f549766f26691883
gekogekominimi <chousokabe1949556@gmail.com>
2020-11-09 11:01:34 +0900

    #37 検索ページ編集

--- resources/views/search/search.blade.php
+++ resources/views/search/search.blade.php
@@ -4,7 +4,8 @@
 </head>
 <body>
     @foreach($result as $item)
-        <p>{{ $item->name }}</p>
+        #<p>{{ $item->name }}</p>
+        <a href=" /user/{id}/profile">{{ $item->name }}</a>
     @endforeach
 </body>
 </html>

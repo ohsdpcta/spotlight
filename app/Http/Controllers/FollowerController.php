740fe5f9ae0c0a519a24e7f6c0954103e041bc15
chocopc123 <0215ryuuku@gmail.com>
2020-11-11 14:58:20 +0900

    セルフフォロー防止、重複フォロー防止、URL直接入力対策 #72

--- app/Http/Controllers/FollowerController.php
+++ app/Http/Controllers/FollowerController.php
@@ -9,10 +9,15 @@ use Illuminate\Support\Facades\Auth;
 class FollowerController extends Controller
 {
     public function follow(Request $request, $id){
-        $follower = new Follower;
-        $follower->target_id = $id;
-        $follower->follower_id = Auth::id();
-        $follower->save();
+        $follow_data = Follower::where('target_id', $id)
+            ->where('follower_id', Auth::id())
+            ->get();
+        if(count($follow_data) == 0 and Auth::id() != $id){
+            $follower = new Follower;
+            $follower->target_id = $id;
+            $follower->follower_id = Auth::id();
+            $follower->save();
+        }
         return redirect("/user/{$id}/profile");
     }
 

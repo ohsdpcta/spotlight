9fbe26e17833423fe3d4bc72a5164d5d4bbeda94
gekogekominimi <chousokabe1949556@gmail.com>
2020-11-12 10:46:14 +0900

    #63 グッズページ機能作成

--- app/Http/Controllers/GoodsController.php
+++ app/Http/Controllers/GoodsController.php
@@ -2,36 +2,41 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
+use App\Goods;
 
 class GoodsController extends Controller
 {
-    public function index(Request $request)
+    public function goods(Request $request, $id)
     {
-        $items = DB::select('select * from goods');
-        return view('goods.index', ['items' => $items]);
+        $data = Goods::where('user_id', $id)->get();
+        return view('goods.goods', compact('data'));
     }
     
-    public function post(Request $request)
-    {
-        $items = DB::select('select * from goods');
-        return view('goods.index', ['items' => $items]);
-    }
-
     public function add(Request $request)
     {
         return view('goods.add');
     }
 
-    public function create(Request $request)
+    public function create(Request $request, $id)
     {
-        $param = [
-            'name' => $request->name,
-            'url' => $request->url,
-        ];
-        DB::insert('insert into goods (name, url) values
-            (:name, :url)', $param);
+        $addgoods = new Goods;
+        $addgoods->user_id = $id;
+        $addgoods->name = $request->name;
+        $addgoods->url = $request->url;
+        $addgoods->save();
+        return redirect("user/{$id}/goods");
+    }
+    public function del(Request $request, $id) {
+        $data = Goods::where('id', $id)->get();
+        return view('Goods.del', compact('data'));
+    }
+
+    public function remove(Request $request, $id) {
+        // レコードを削除する。
+        $return = Goods::find($id);
+        Goods::where('id', $id)->delete();
+        return redirect("/user/{$return->user_id}/goods");
     }
 }
 
--- resources/views/goods/add.blade.php
+++ resources/views/goods/add.blade.php
@@ -1,12 +1,10 @@
-@extends('layouts.main')
+@extends('layouts.user')
 
 @section('content')
-    <form action="/user/{id}/goods" method="post">
-        <table>
-            @csrf
-            <tr><th>name: </th><td><input type="text" name="name"></td></tr>
-            <tr><th>url: </th><td><input type="text" name="url"></td></tr>
-            <tr><th></th><td><input type="submit" value="登録"></td></tr>
-        </table>
+    <form action="add" method="post">
+        @csrf
+            グッズ名:<input type="text" name="name"><br>
+        　　 URL:<input type="text" name="url"><br><br>
+        <input type="submit" value="登録">
     </form>
 @endsection

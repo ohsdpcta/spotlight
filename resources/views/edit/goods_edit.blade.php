cbdad70140ea5b959b2fa4f5706146360ceb8883
gekogekominimi <chousokabe1949556@gmail.com>
2020-11-13 14:48:59 +0900

    #91 ユーザー編集画面

--- resources/views/edit/goods_edit.blade.php
+++ resources/views/edit/goods_edit.blade.php
@@ -11,8 +11,11 @@
     <p><a href="goods/add">商品の追加</a></p><br>
 
     @foreach($data as $item)
-        <p><a href="{{ $item->url }}">{{ $item->name }}　　
-        </a><a href="/user/{{ $item->id }}/goods/del">削除</p>
+        <p><input type="checkbox" name="check" value="{{$item->id}}">
+        <a href="{{ $item->url }}">{{ $item->name }}</a>
+        <a href="/user/{{request()->id}}/goods/{{$item->id}}/del">削除</p>
     @endforeach
+        {{--{{request()->id()}}でURLのユーザーID取得  --}}
+        <p><a href="/user/{{request()->id}}/goods/multi_del">選択削除</p>
 </body>
 </html>

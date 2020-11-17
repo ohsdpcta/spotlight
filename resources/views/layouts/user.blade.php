bc20ed58714b5c112354e4c1682fd74c098dc204
tajisan <tajima1949544@gmail.com>
2020-11-17 12:06:52 +0900

    ユーザーページのレイアウト #121

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -4,19 +4,41 @@
 <div class="container">
 
     <div class="top">
-        <img src="http://placehold.jp/200x200.png" class="rounded-circle">
-        {{-- ユーザー名 --}}
-        <h1>{{ UserClass::getUser(request()->id)->name }}</h1>
-        {{-- フォローボタン表示 --}}
-        @if(Auth::user() and Auth::user()->id != request()->id)
-            @if(UserClass::getFollower(request()->id)['follow_flg'] == 1)
-                <input type="button" onclick="location.href='/user/{{ request()->id }}/unfollow'" value="フォロー解除">
-            @else
-                <input type="button" onclick="location.href='/user/{{ request()->id }}/follow'" value="フォロー">
-            @endif
-        @endif
-        {{-- フォロワー数 --}}
-        <p>フォロワー: {{ UserClass::getFollower(request()->id)['follower'] }}</p>
+        <div class="row">
+            <div class="col-md-3 pt-2 pl-4">
+                <img src="http://placehold.jp/200x200.png" class="rounded-circle">
+            </div>
+            {{-- ユーザー名 --}}
+            <div class="col-4 pt-2 pr-2 pb-2 pl-1">
+                <h1>{{ UserClass::getUser(request()->id)->name }}</h1>
+                {{-- タグ(仮) --}}
+                <div class="pt-4">
+                    <h5>タグ挿入予定地(仮)</h5>
+                </div>
+            </div>
+            {{-- フォローボタン表示 --}}
+            <div class="col-4 pt-3 pl-1">
+            {{-- @if(Auth::user() and Auth::user()->id != request()->id)あとで表示 --}}
+                @if(UserClass::getFollower(request()->id)['follow_flg'] == 1)
+                    <input type="button" onclick="location.href='/user/{{ request()->id }}/unfollow'" class="btn btn-primary" value="フォロー解除">
+                @else
+                    <input type="button" onclick="location.href='/user/{{ request()->id }}/follow'" class="btn btn-primary" value=" フォロー ">
+                @endif
+                <div class="pt-1">
+                    <input type="button" onclick="location.href='/user/{{request()->id}}/edit/user'" class="btn btn-success" value="プロフィール編集">
+                </div>
+            {{-- @endif後で表示 --}}
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-1 col-md-3"></div>
+            {{-- フォロワー数 --}}
+            <div class="col-1 col-md-6 pl-1">
+                <p>フォロー中: {{ UserClass::getFollower(request()->id)['follower'] }}　　　
+                   フォロワー: {{ UserClass::getFollower(request()->id)['follower'] }}</p>
+            </div>
+            
+        </div>
     </div>
 
     <div class="tab w-100 nav-justified">

23c7137f48d8ca4e15c4e61d05ab3becf3765ba3
chocopc123 <0215ryuuku@gmail.com>
2020-11-18 10:44:21 +0900

    GoodsTableSeederを作成 Fixes #117

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
             ProfileTableSeeder::class,
             FollowersTableSeeder::class,
             LocateTableSeeder::class,
+            GoodsTableSeeder::class,
         ]);
     }
 }

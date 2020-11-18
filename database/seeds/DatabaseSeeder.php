829497c0764d7101feec9e9f3f6ab32a250524e1
chocopc123 <0215ryuuku@gmail.com>
2020-11-18 11:00:09 +0900

    SampleTableSeederを作成 #118

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
             FollowersTableSeeder::class,
             LocateTableSeeder::class,
             GoodsTableSeeder::class,
+            SampleTableSeeder::class,
         ]);
     }
 }

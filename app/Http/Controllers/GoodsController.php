f535fe0e92cfc57f07b041b1e188d95abdd11d9f
tajisan <tajima1949544@gmail.com>
2020-11-20 10:30:31 +0900

    GoodsControllerにページネーションが反映されるように修正

--- app/Http/Controllers/GoodsController.php
+++ app/Http/Controllers/GoodsController.php
@@ -11,7 +11,7 @@ class GoodsController extends Controller
 {
     // 一覧
     public function goods(Request $request, $id){
-        $data = Goods::where('user_id', $id)->get();
+        $data = Goods::find($id)->paginate(10);
         return view('goods.goods', compact('data'));
     }
     //新規追加

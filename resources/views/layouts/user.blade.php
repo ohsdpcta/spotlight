0f3fbbeff931454563026982530498b5d3b46c35
HN-00 <h.naoki00@gmail.com>
2020-12-06 01:56:12 +0900

    編集ボタンやpaypayボタンもいじろうと思った

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -15,7 +15,7 @@
             </div>
 
             {{-- ユーザー名 --}}
-            <div class="border col-xl-7 col-lg-6 col-md-8 col-sm-12 col-xs-12 pt-2 pr-2 pb-2 pl-2">
+            <div class="border col-xl-6 col-lg-6 col-md-8 col-sm-12 col-xs-12 pt-2 pr-2 pb-2 pl-2">
                 <h1>{{ UserClass::getUser(request()->id)->name }}</h1>
                 {{-- タグ(仮) --}}
                 <div class="pt-4">
@@ -24,17 +24,20 @@
             </div>
 
             {{-- フォローボタン, 編集ページリンク等…… --}}
-            <div class="border col-xl-2 col-lg-2 col-md-12 col-sm-2 pt-3 pl-1">
+            <div class="border col-xl-2 col-lg-2 col-md-12 col-sm-12">
+                <!-- フォローボタン -->
                 @if(Auth::user() and Auth::user()->id != request()->id)
-                    @if(UserClass::getFollower(request()->id)['follow_flg'] == 1)
-                        <input type="button" onclick="location.href='/user/{{ request()->id }}/unfollow'" class="btn btn-primary" value="フォロー解除">
-                    @else
-                        <input type="button" onclick="location.href='/user/{{ request()->id }}/follow'" class="btn btn-primary" value=" フォロー ">
-                    @endif
-
+                    <div class="col-3">
+                        @if(UserClass::getFollower(request()->id)['follow_flg'] == 1)
+                            <input type="button" onclick="location.href='/user/{{ request()->id }}/unfollow'" class="btn btn-primary" value="フォロー解除">
+                        @else
+                            <input type="button" onclick="location.href='/user/{{ request()->id }}/follow'" class="btn btn-primary" value=" フォロー ">
+                        @endif
+                    </div>
+                    
                 {{-- ユーザーページ編集 --}}
                 @else
-                    <div class="">
+                    <div class="col-3 pt-2">
                         <input class="btn btn-success" value="ユーザーページ編集" type="button"
                             onclick="location.href='/user/{{Auth::id()}}/summary/@if(request()->is('*profile'))profile\
                             @elseif(request()->is('*locate'))locate\
@@ -47,7 +50,7 @@
 
                 {{-- 投げ銭ボタン --}}
                 @if( !empty(UserClass::get_paypay_url(request()->id)) )
-                    <div class="pt-2">
+                    <div class="col-3 pt-2 pb-2">
                         <a href="/user/{{request()->id}}/tip"><img src="https://iconlab.kentakomiya.com/wp/wp-content/uploads/2019/06/icon0084.png" alt="投げ銭" width="30" height="30"></a>
                     </div>
                 @endif
@@ -64,7 +67,7 @@
         
     </div>
 
-    <div class="tab">
+    <div class="tab nav-justified">
         <ul class="nav nav-tabs">
             <li class="nav-item">
                 <a class="nav-link @if(request()->is('*profile')) active @endif" href="/user/{{request()->id}}/profile">プロフィール</a>

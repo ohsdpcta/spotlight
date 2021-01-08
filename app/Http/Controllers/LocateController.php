75706e23fc5400a4ba818f9e60545162edd3608b
tajisan <73516711+tajisan@users.noreply.github.com>
2021-01-08 10:28:22 +0900

    Update LocateController.php

--- app/Http/Controllers/LocateController.php
+++ app/Http/Controllers/LocateController.php
@@ -5,10 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
 
 use App\Locate;
-
-use Illuminate\Validation\Rule;
 
 class LocateController extends Controller
 {

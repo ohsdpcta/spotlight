c30fec8b7254f1c8aa7d2be72b33b3dcc390e230
chocopc123 <0215ryuuku@gmail.com>
2021-01-20 14:43:03 +0900

    ドロワーメニューのアイコンを変更

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -114,7 +114,11 @@
         </script>
 
         <div class="side-content">
-            <button type="button" class="btn_menu"><i class="fas fa-bars"></i></button>
+            <button type="button" class="btn btn-info btn_menu">
+                <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M2.5 11.5A.5.5 0 0 1 3 11h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 7h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 3h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+                </svg>
+            </button>
             <!-- サイド(ドロワー)メニュー https://296.co.jp/article/09392320181809143 -->
             <nav class="border container sidebar text-dark">
                 <br>

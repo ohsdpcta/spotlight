14dad15ce380a460f298315ff51a7a57767887bd
tajisan <tajima1949544@gmail.com>
2021-01-25 11:33:03 +0900

    #309 ユーザーが活動地域(都道府県~市区まで)を登録できるように

--- resources/views/summary/edit_locate.blade.php
+++ resources/views/summary/edit_locate.blade.php
@@ -36,8 +36,9 @@
                 @csrf
                 {{-- 各種フォーム入力欄 --}}
                 {{-- バリデーションエラーがあった場合は、old関数で入力データを復元する --}}
+                <label>活動地域</label><input type="text" name="prefecture_city" class="form-control" value="{{ $locate_data->prefecture }}{{ $locate_data->city }}" placeholder="登録する住所を入力してください。例:東京都品川区"><br>
                 <label>活動場所(座標)</label><br>
-                <input type="text" id="latlng" class="form-control col-lg-12 col-md-12 col-sm-12 col-xs-12" name="coordinate" value="{{old('coordinate')}}" placeholder="登録したい住所の座標を入力してください。">
+                <input type="text" id="latlng" class="form-control col-lg-12 col-md-12 col-sm-12 col-xs-12" name="coordinate" value="{{old('coordinate')}}" placeholder="登録する住所の座標を入力してください。">
                 {{-- 各種ボタン --}}
                 <div class="float-lg-right float-md-right float-sm-right float-xs-right pt-1 pb-3">
                     @if($locate_array)

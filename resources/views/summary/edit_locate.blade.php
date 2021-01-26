2a3fbe8d5b317bf18cdec1a1aa60ee27977e0de4
chocopc123 <0215ryuuku@gmail.com>
2021-01-26 10:53:40 +0900

    feat: ロケーション登録時に住所を取得

--- resources/views/summary/edit_locate.blade.php
+++ resources/views/summary/edit_locate.blade.php
@@ -32,18 +32,19 @@
             </div>
         @endif
         <div>
-            <form action="/user/{{Auth::id()}}/summary/locate" method="post">
+            <form action="/user/{{Auth::id()}}/summary/locate" method="post" name="locate_form">
                 @csrf
                 {{-- 各種フォーム入力欄 --}}
                 <label>活動場所(座標)</label><br>
                 <input type="text" id="latlng" class="form-control col-lg-12 col-md-12 col-sm-12 col-xs-12" name="coordinate" value="{{old('coordinate')}}" placeholder="登録したい住所の座標を入力してください。">
+                <input type="text" id="address" name="address" hidden>
                 {{-- 各種ボタン --}}
                 <div class="float-lg-right float-md-right float-sm-right float-xs-right pt-1 pb-3">
                     @if($locate_array)
-                        <input type="submit" value="修正" class="btn btn-success">
+                        <input type="submit" value="修正" id="save" class="btn btn-success">
                         <button type="button" class="btn btn-danger" onclick="location.href='/user/{{Auth::id()}}/summary/locate/delete'">削除</button>
                     @else
-                        <input type="submit" value="登録" class="btn btn-primary">
+                        <input type="submit" value="登録" id="save" class="btn btn-primary">
                     @endif
                 </div>
             </form>
@@ -52,21 +53,24 @@
     <div id="map" class="col-lg-12 col-md-12 col-sm-12 col-xs-12"></div><br>
 
     <script>
+        const geocoder = new google.maps.Geocoder();
+        const save_btn = document.getElementById('save');
         const [default_lat, default_lng, default_zoom] = setDefaultProperty();
+
         map = new GMaps({
             div: '#map', //地図を表示する要素
             lat: default_lat, // 緯度
             lng: default_lng, // 経度
             zoom: default_zoom,   //倍率（1～21）
         });
-        map.addMarker({
-            lat: default_lat,
-            lng: default_lng,
-        });
 
         map.addListener('click', function(e) {
             getClickLatLng(e.latLng, map);
         });
+        save_btn.addEventListener('click', function(e) {
+            e.preventDefault();
+            getAddress();
+        })
 
         function getClickLatLng(lat_lng, map) {
             document.getElementById('latlng').value = lat_lng.lat() + ',' + lat_lng.lng();
@@ -77,6 +81,11 @@
                 lng: lat_lng.lng(),
             });
         }
+
+        map.addMarker({
+            lat: default_lat,
+            lng: default_lng,
+        });
 
         function setDefaultProperty(){
             let lat, lng;
@@ -91,6 +100,27 @@
                 zoom = 5;
             }
             return [lat, lng, zoom];
+        }
+
+        function getAddress(){
+            const input = document.getElementById("latlng").value;
+            const latlngStr = input.split(",", 2);
+            const latlng = {
+                lat: parseFloat(latlngStr[0]),
+                lng: parseFloat(latlngStr[1]),
+            };
+            geocoder.geocode({ location: latlng }, (results, status) => {
+                if (status === "OK") {
+                    if (results[0]) {
+                        document.getElementById('address').value = results[0].formatted_address;
+                    } else {
+                        window.alert("住所を特定できませんでした");
+                    }
+                } else {
+                    window.alert("住所を特定できませんでした");
+                }
+                document.locate_form.submit();
+            });
         }
     </script>
 </html>

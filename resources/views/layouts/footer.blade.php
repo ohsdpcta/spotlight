874c9a2400ea9be1c2e4adbc0b389da8c28dcd30
chocopc123 <0215ryuuku@gmail.com>
2021-01-28 11:56:17 +0900

    ref: footerを別ファイルに分割

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,28 @@
+<footer class="footer sticky-bottom bg-light">
+    <div class="container">
+        <div class="row pt-2 pb-2 pr-2 pl-2">
+            <div class="col-4 text-muted">
+                <h5>about us...</h5>
+                <p>D'où Venons Nous Que Sommes Nous Où Allons Nous</p>
+            </div>
+            <div class="col-4 text-muted">
+                <h5>navigation</h5>
+                <ul>
+                    <li>top</li>
+                    <li>search</li>
+                    <li>signup</li>
+                    <li>signin</li>
+                    <li>mypage</li>
+                </ul>
+            </div>
+            <div class="col-4 text-muted">
+                <h5 class="text-muted">contact info</h5>
+                <ul>
+                    <li>☎</li>
+                    <li>〒</li>
+                    <li>✉</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>

944ce289d355d90aa3b489b1cbfa28923b2ecf04
UY0409 <utikawa1949592@gmail.com>
2021-02-01 14:05:11 +0900

    ビュー修正

--- resources/views/emails/social_change.blade.php
+++ resources/views/emails/social_change.blade.php
@@ -5,10 +5,10 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <title>ソーシャルID変更メール送信ページ　/　Spotlight</title>
+        <title>ソーシャルID変更ページ　/　Spotlight</title>
     </head>
     @section('R_form')
-        <p>ソーシャルID変更メールの送信を行いますか？</p>
+        <p>ソーシャルID変更を行いますか？</p>
     <form action="socialemail" method="POST">
         @csrf
             <label><input type="submit" name="send" value="送信"> </label>

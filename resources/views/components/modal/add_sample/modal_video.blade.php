5b62830bb2d6656b2a3445abcd98a74e4ec0a609
tajisan <tajima1949544@gmail.com>
2021-02-03 09:44:12 +0900

    ボーダルウィンドウを追加+記述を変更

---
+++ resources/views/components/modal/add_sample/modal_video.blade.php
@@ -0,0 +1,21 @@
+@include('components/sample_info', ['modal_name' => 'modal_video', 'sample_modal_content' => '
+    <h3 class="pt-3">サンプルの利用方法</h3>
+    <hr>
+    <h6 class="pb-3">あなたの動画やプレイリストを埋め込むことができます。<br>
+        埋め込みたいメディアのボタンをクリックして下さい。</h6>
+    <p>youtubeの動画を埋め込む...<br>
+        <a href="https://stat.ameba.jp/user_images/20210118/13/tajitaji2121/88/d3/j/o1283091314883367026.jpg?caw=800" data-lightbox="youtube"><button class="btn btn-danger">YouTube</button></a>
+        <a href="https://stat.ameba.jp/user_images/20210118/13/tajitaji2121/50/cb/j/o0807061114883367062.jpg?caw=800" data-lightbox="youtube"></a>
+        <a href="https://stat.ameba.jp/user_images/20210118/13/tajitaji2121/59/d8/j/o0846069214883367070.jpg?caw=800" data-lightbox="youtube"></a>
+        <a href="https://stat.ameba.jp/user_images/20210118/13/tajitaji2121/8b/77/j/o1022044514883367100.jpg?caw=800" data-lightbox="youtube"></a>
+    </p>
+    <p>youtubeの再生リストを埋め込む...<br>
+        <a href="https://stat.ameba.jp/user_images/20210118/14/tajitaji2121/08/f8/j/o1097065714883387205.jpg?caw=800" data-lightbox="youtube_list"><button class="btn btn-danger">YouTube_list</button></a>
+        <a href="https://stat.ameba.jp/user_images/20210118/13/tajitaji2121/8b/77/j/o1022044514883367100.jpg?caw=800" data-lightbox="youtube_list"></a>
+    </p>
+    <p>soundcloudのプレイリストを埋め込む...<br>
+        <a href="https://stat.ameba.jp/user_images/20210118/14/tajitaji2121/dd/81/j/o0953067314883387213.jpg?caw=800" data-lightbox="soundcloud"><button class="btn btn-primary">SoundCloud</button></a>
+        <a href="https://stat.ameba.jp/user_images/20210118/14/tajitaji2121/4c/cb/j/o0772054214883387224.jpg?caw=800" data-lightbox="soundcloud"></a>
+        <a href="https://stat.ameba.jp/user_images/20210118/13/tajitaji2121/8b/77/j/o1022044514883367100.jpg?caw=800" data-lightbox="soundcloud"></a>
+    </p>'
+])

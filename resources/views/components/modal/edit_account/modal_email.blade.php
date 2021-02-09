c650b7782142bfcbfd43533bba6d83c32657e96b
tajisan <tajima1949544@gmail.com>
2021-02-09 11:59:06 +0900

    #315 各種アカウント情報変更をモーダルウィンドウでの表示に変更

--- resources/views/components/modal/edit_account/modal_email.blade.php
+++ resources/views/components/modal/edit_account/modal_email.blade.php
@@ -1,5 +1,23 @@
-@include('components/user_info', ['modal_name' => 'modal_email', 'user_modal_content' => '
-    <br><h3 class="pt-3">メールサンプル</h3>
+@include('components/user_change', ['modal_name' => 'modal_email', 'change_name' => 'メールアドレス変更', 'btn_color' => 'primary', 'user_modal_content' => '
+    <br><h3>メールアドレス変更</h3>
     <hr>
-    <h6 class="font-weight-bold">メールサンプル</h6>'
+    <p>新しいメールアドレスを入力してください</p>
+    <table>
+        <tr>
+            <th></th>
+            <th></th>
+        </tr>
+        <tr>
+            <div class="form-group">
+                <td>新しいメールアドレス::</td><td><input type="text"  class="form-control" name="new_mail" value=""></td>
+            </div>
+        <tr>
+        <tr>
+            <div class="form-group">
+                <td>新しいメールアドレスの確認::</td><td><input type="text" class="form-control" name="new_mail_confirmation" value=""></td>
+            </div>
+        </tr>
+    </table>
+    <label><input type="submit" class="btn btn-primary" name="send" value="変更"> </label>
+    '
 ])
